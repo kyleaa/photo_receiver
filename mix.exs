50c2ccbc61dc9c3e044d55c9ef81041bdc81c2f0
Adam Badawy <adambadawy@users.noreply.github.com>
2016-08-23 15:28:09 -0400

    Bump version
    pls

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhotoReceiver.Mixfile do
 
   def project do
     [app: :photo_receiver,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
